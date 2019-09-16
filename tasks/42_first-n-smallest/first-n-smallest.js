bfd34656e65705223c216d8dcaa9a60bc8bd6594
jkozeeva <jkozeeva@griddynamics.com>
2019-09-16 12:30:44 +0400

    change func on arrow func in tasks 42

--- tasks/42_first-n-smallest/first-n-smallest.js
+++ tasks/42_first-n-smallest/first-n-smallest.js
@@ -1,10 +1,8 @@
 function firstNSmallest(array, n) {
   const result = [];
-  const sortedArr = array.slice().sort(function(a, b) {
-    return a - b;
-  });
+  const sortedArr = array.slice().sort((a, b) => a - b);
   const smallestNumbers = sortedArr.slice(0, n);
-  array.forEach(function(num) {
+  array.forEach((num) => {
     if (smallestNumbers.includes(num)) {
       result.push(num);
     }
