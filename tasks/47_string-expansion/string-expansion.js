4e3d4313668ce4b1c1ed5c0938f51005c9ff4113
jkozeeva <jkozeeva@griddynamics.com>
2019-09-16 15:08:52 +0400

    add JSDoc for task 47

--- tasks/47_string-expansion/string-expansion.js
+++ tasks/47_string-expansion/string-expansion.js
@@ -1,3 +1,14 @@
+/**
+ * stringExpansion return the expansion of that string:
+ * The numeric values represent the occurrence of each letter
+ * preceding that numeric value
+ * @param  {String} str
+ * @param  {RegExp} regLetter
+ * @param  {RegExp} regNumber
+ * @param {String} newStr
+ * @param {Number}  charIndex
+ * @return  {String} Returns the value
+ */
 function stringExpansion(str) {
   const regLetter = new RegExp(/^[A-Za-z]+$/);
   const regNumber = new RegExp(/^[0-9]$/);
@@ -7,10 +18,10 @@ function stringExpansion(str) {
   let newStr = '';
   for (let i = 0; i < str.length; i++) {
     if (regNumber.test(str[i])) {
-      let j = 1;
-      while ((regLetter.test(str[i+j])) && (i+j)<str.length) {
-        newStr += str[i+j].repeat(Number(str[i]));
-        j++;
+      let charIndex = 1;
+      while ((regLetter.test(str[i+charIndex])) && (i+charIndex)<str.length) {
+        newStr += str[i+charIndex].repeat(Number(str[i]));
+        charIndex++;
       }
     }
   }
