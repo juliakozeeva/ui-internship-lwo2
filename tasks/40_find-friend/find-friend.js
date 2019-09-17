7b293b1a683bb5d55731093bbd3272138dcb2dfe
jkozeeva <jkozeeva@griddynamics.com>
2019-09-17 12:25:36 +0400

    fix task 40

--- tasks/40_find-friend/find-friend.js
+++ tasks/40_find-friend/find-friend.js
@@ -2,35 +2,15 @@ function findFriend(arr) {
   const redMask = 'red';
   const blueMask = 'blue';
   let counter = 0;
-  for (let i = 2; i < arr.length - 2; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] === redMask) {
-      if (arr[i-1] === blueMask && arr[i+1] === blueMask) {
+      if (arr[i - 1] === blueMask && arr[i + 1] === blueMask) {
         counter++;
-      } else if (arr[i-1] === blueMask && arr[i-2] === blueMask) {
+      } else if (arr[i - 1] === blueMask && arr[i - 2] === blueMask) {
         counter++;
-      } else if (arr[i+1] === blueMask && arr[i+2] === blueMask) {
+      } else if (arr[i + 1] === blueMask && arr[i + 2] === blueMask) {
         counter++;
       }
-    }
-  }
-  if (arr[0] === redMask) {
-    if (arr[1] === blueMask && arr[2] === blueMask) {
-      counter++;
-    }
-  }
-  if (arr[1] === redMask) {
-    if (arr[0] === blueMask && arr[2] === blueMask) {
-      counter++;
-    }
-  }
-  if (arr[arr.length-1] === redMask ) {
-    if (arr[arr.length-2] === blueMask && arr[arr.length-3] === blueMask) {
-      counter++;
-    }
-  }
-  if (arr[arr.length-2] === redMask) {
-    if (arr[arr.length-1] === blueMask && arr[arr.length-3] === blueMask) {
-      counter++;
     }
   }
   return counter;
